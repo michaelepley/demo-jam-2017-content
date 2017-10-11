90fa562c8371bc11681e09dcb18506bd0e7d6d78
mepley <mepley@breccia.home>
2017-10-11 09:46:28 -0400

    Added image build information for arena s2i image

---
+++ images/arena-viewer/dockercontext/resources/setup-vnc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "	--> setup the Xvnc password"
+mkdir -p ~/.vnc && echo "VNCPASS" | vncpasswd -f > ~/.vnc/passwd && chmod 600 ~/.vnc/passwd && chmod 740 ~/.vnc
+
+# create the xstartup file for Xvnc
+cat > ~/.vnc/xstartup <<EOF1
+openbox-session &
+EOF1
+
+chmod a+x ~/.vnc/xstartup
+
+# set openbox to launch JBDS at startup
+mkdir -p ~/.config/openbox ~/workspace
+
+cat > ~/.config/openbox/environment <<EOF2
+export DISPLAY=:1
+EOF2
+
+cat > ~/.config/openbox/autostart <<EOF3
+/usr/share/jbdevstudio/jbdevstudio -nosplash -data $HOME/workspace &
+EOF3
+
---
+++ images/arena-viewer/dockercontext/resources/start.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+set -e
+
+# A single argument can be passed to set the desired screen size.
+# 	The argument must be in the form HORZxVERT where HORZ is a
+# 	number >= 640 and VERT is a number >= 480.
+
+# default screen size
+SCREEN_SIZE="1440x726"
+
+# waits until desired window, with name in $1, is active
+function wait_for_window {
+    while [ "x`DISPLAY=:1 wmctrl -l | grep "$1"`" = "x" ]
+    do
+        sleep 2
+    done
+}
+
+# trap signals so we can close cleanly
+RUNNING=true
+trap "RUNNING=false" HUP INT QUIT KILL TERM
+
+# set screen size based on arg if valid
+if [ "$#" -gt 0 ]
+then
+  if [ "x`echo $1 | grep -E '^[0-9]+x[0-9]+$'`" != "x" ]
+  then
+    HORZ=`echo $1 | cut -dx -f1`
+    VERT=`echo $1 | cut -dx -f2`
+
+    if [ "$HORZ" -ge 640 -a "$VERT" -ge 480 ]
+    then
+      SCREEN_SIZE="${HORZ}x${VERT}"
+    fi
+  fi
+fi
+
+# create all needed directories
+cd $HOME
+mkdir -p workspace .vnc
+
+# set vnc config if missing
+cd .vnc
+if [ ! -f passwd ]
+then
+  chmod 740 .
+  echo 'VNCPASS' | vncpasswd -f > passwd
+  chmod 600 passwd
+  echo 'openbox-session &' > xstartup
+  chmod a+x xstartup
+fi
+
+echo "Screen resolution set to $SCREEN_SIZE"
+
+echo "Launching  Xvnc which launches openbox and JBDS ..."
+cd ..
+vncserver :1 -name 'Desktop Name' -geometry $SCREEN_SIZE -depth 24
+
+echo "Making application fullscreen ..."
+dummy=$(wait_for_window ' - Red Hat JBoss Developer Studio' && :)
+jbdswin=`DISPLAY=:1 wmctrl -l | \
+    grep 'Applet viewer' | \
+    cut -d' ' -f1`
+DISPLAY=:1 wmctrl -i -r $jbdswin -b add,fullscreen
+
+echo "Loop to prevent container from exiting"
+echo "CTRL-C to exit or run 'docker stop <container>'"
+while $RUNNING ; do  sleep 2; done
+
+echo "Closing 'cleanly' by killing Xvnc ..."
+vncserver -kill :1
+
+# remove user restricted files so they can be recreated
+rm -f passwd .vnc
+
+# set all permissions for any user
+dummy=$(chmod -R a+rwX ${HOME}/* && :)
+
